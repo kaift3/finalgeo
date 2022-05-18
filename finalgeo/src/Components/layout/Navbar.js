19ef6f74dfb8e7fb78fd26733241ebe691a3ee2f
pratikstemkar <pratikstemkar@gmail.com>
2022-05-18 17:36:07 +0530

    Route

--- finalgeo/src/Components/layout/Navbar.js
+++ finalgeo/src/Components/layout/Navbar.js
@@ -3,132 +3,132 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Contexts/AuthContext";
 
 const Navbar = () => {
-  const { user, logout } = useContext(AuthContext);
-  const navigate = useNavigate();
+	const { user, logout } = useContext(AuthContext);
+	const navigate = useNavigate();
 
-  return (
-    <>
-      <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-              GIS Application
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              {localStorage.getItem("token") ? (
-                <>
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="/">
-                        Home
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="/about">
-                        About
-                      </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="navbarDropdown"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        Geo
-                      </a>
-                      <ul
-                        class="dropdown-menu"
-                        aria-labelledby="navbarDropdown"
-                      >
-                        <li>
-                          <a class="dropdown-item" href="/geotag">
-                            GeoTag
-                          </a>
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="/geofence">
-                            GeoFence
-                          </a>
-                        </li>
-                        <li>
-                          <hr class="dropdown-divider" />
-                        </li>
-                        {/* <li>
+	return (
+		<>
+			<header>
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+					<div class="container-fluid">
+						<a class="navbar-brand" href="/">
+							GIS Application
+						</a>
+						<button
+							class="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation"
+						>
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							{localStorage.getItem("token") ? (
+								<>
+									<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+										<li class="nav-item">
+											<a class="nav-link" aria-current="page" href="/">
+												Home
+											</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="/about">
+												About
+											</a>
+										</li>
+										<li class="nav-item dropdown">
+											<a
+												class="nav-link dropdown-toggle"
+												href="#"
+												id="navbarDropdown"
+												role="button"
+												data-bs-toggle="dropdown"
+												aria-expanded="false"
+											>
+												Geo
+											</a>
+											<ul
+												class="dropdown-menu"
+												aria-labelledby="navbarDropdown"
+											>
+												<li>
+													<a class="dropdown-item" href="/geotag">
+														GeoTag
+													</a>
+												</li>
+												<li>
+													<a class="dropdown-item" href="/geofence">
+														GeoFence
+													</a>
+												</li>
+												<li>
+													<hr class="dropdown-divider" />
+												</li>
+												<li>
 													<a class="dropdown-item" href="/routes">
 														Routes
 													</a>
-												</li> */}
-                      </ul>
-                    </li>
-                  </ul>
-                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    {/* <li class="nav-item">
+												</li>
+											</ul>
+										</li>
+									</ul>
+									<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+										{/* <li class="nav-item">
 											<span class="nav-link" href="#">
 												Hello {user.name !== null ? user.name : null}
 											</span>
 										</li> */}
-                    <li class="nav-item">
-                      <a
-                        class="nav-link"
-                        href="#"
-                        onClick={() => {
-                          logout();
-                          navigate("/login");
-                        }}
-                      >
-                        Logout
-                      </a>
-                    </li>
-                  </ul>
-                </>
-              ) : (
-                <>
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="/">
-                        Home
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="/about">
-                        About
-                      </a>
-                    </li>
-                  </ul>
-                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="/register">
-                        Register
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="/login">
-                        Login
-                      </a>
-                    </li>
-                  </ul>
-                </>
-              )}
-            </div>
-          </div>
-        </nav>
-      </header>
-    </>
-  );
+										<li class="nav-item">
+											<a
+												class="nav-link"
+												href="#"
+												onClick={() => {
+													logout();
+													navigate("/login");
+												}}
+											>
+												Logout
+											</a>
+										</li>
+									</ul>
+								</>
+							) : (
+								<>
+									<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+										<li class="nav-item">
+											<a class="nav-link" aria-current="page" href="/">
+												Home
+											</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="/about">
+												About
+											</a>
+										</li>
+									</ul>
+									<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+										<li class="nav-item">
+											<a class="nav-link" aria-current="page" href="/register">
+												Register
+											</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="/login">
+												Login
+											</a>
+										</li>
+									</ul>
+								</>
+							)}
+						</div>
+					</div>
+				</nav>
+			</header>
+		</>
+	);
 };
 
 export default Navbar;
--- finalgeo/src/Pages/ShortestRoutes.js
+++ finalgeo/src/Pages/ShortestRoutes.js
@@ -1,98 +1,183 @@
-import React, { useState } from "react";
-import Sidebar from "../Components/Sidebar";
-import tags from "../Data/List";
+import React, { useEffect, useState } from "react";
+import * as L from "leaflet";
+import {
+	MapContainer,
+	LayersControl,
+	TileLayer,
+	Marker,
+	Popup,
+	useMapEvents,
+} from "react-leaflet";
+import "leaflet-routing-machine";
+import { SearchControl, OpenStreetMapProvider } from "react-leaflet-geosearch";
+import "leaflet-geosearch/assets/css/leaflet.css";
 
 import "./GeoTag.css";
 
 const GeoTag = () => {
-	const [openLeft, setOpenLeft] = useState(false);
-	const [openRight, setOpenRight] = useState(false);
+	const [state, setState] = useState({
+		map: null,
+		markers: [],
+		flag: false,
+	});
 
-	const toggleDrawerLeft = () => {
-		setOpenLeft(!openLeft);
+	const GeoSearchControlElement = SearchControl;
+	const prov = OpenStreetMapProvider();
+
+	var myIcon = L.icon({
+		iconUrl:
+			"https://cdn.discordapp.com/attachments/909801322436505600/933675374628438016/forest-fire.png",
+
+		iconSize: [30, 30],
+	});
+
+	var carIcon = L.icon({
+		iconUrl:
+			"https://cdn.discordapp.com/attachments/511846277554896926/976448671484899348/NicePng_marker-circle-png_1015767.png",
+		iconSize: [30, 30],
+	});
+
+	function LocationMarker() {
+		const map = useMapEvents({
+			click(e) {
+				// console.log(e.latlng);
+
+				if (state.markers.length < 2) {
+					setState((prevState) => ({
+						...prevState,
+						markers: [...prevState.markers, [e.latlng.lat, e.latlng.lng]],
+					}));
+				}
+			},
+		});
+
+		return state.markers !== null
+			? state.markers.map((m, index) => (
+					<>
+						<Marker position={m} icon={myIcon}>
+							<Popup>Point {index}</Popup>
+						</Marker>
+					</>
+			  ))
+			: null;
+	}
+
+	const resetMarkers = () => {
+		setState((prevState) => ({
+			...prevState,
+			markers: [],
+			flag: false,
+		}));
+		window.location.reload();
 	};
 
-	const toggleDrawerRight = () => {
-		setOpenRight(!openRight);
+	const getRoute = (map) => {
+		var marker = L.marker(state.markers[0], { icon: carIcon }).addTo(map);
+		L.Routing.control({
+			waypoints: [L.latLng(state.markers[0]), L.latLng(state.markers[1])],
+		})
+			.on("routesfound", function (e) {
+				var routes = e.routes;
+				console.log(routes);
+
+				e.routes[0].coordinates.forEach(function (coord, index) {
+					setTimeout(function () {
+						marker.setLatLng([coord.lat, coord.lng]);
+					}, 100 * index);
+				});
+			})
+			.addTo(map);
+	};
+
+	const setFlag = (val) => {
+		setState((prevState) => ({
+			...prevState,
+			flag: val,
+		}));
 	};
 
 	return (
 		<>
-			<Sidebar anchor={"left"} open={openLeft} toggleDrawer={toggleDrawerLeft}>
-				<div className="left-sidebar" style={{ backgroundColor: "#212121" }}>
-					<div
-						style={{
-							position: "fixed",
-							backgroundColor: "#212121",
-							boxSizing: "border-box",
-							width: "30vw",
-						}}
-					>
-						<h1
-							style={{
-								color: "white",
-								boxSizing: "border-box",
-							}}
-						>
-							All Tags
-						</h1>
-					</div>
-					<ul style={{ paddingTop: "10vh" }}>
-						{/* <li>
-              <div>
-                <h2>kaif home</h2>
-                <h5>kaift3</h5>
-              </div>
-            </li>
-            <li>
-              <div>
-                <h2>anurag home</h2>
-                <h5>kaift3</h5>
-              </div>
-            </li>
-            <li>
-              <div>
-                <h2>samiran home</h2>
-                <h5>kaift3</h5>
-              </div>
-            </li> */}
-						{tags.map((e, index) => {
-							return (
-								<div
-									key={e.name + "-" + index}
-									style={{ backgroundColor: "#212121" }}
-								>
-									<li>
-										<div>
-											<h2>{e.location}</h2>
-											<h5>{e.name}</h5>
-										</div>
-									</li>
-								</div>
-							);
-						})}
-					</ul>
-				</div>
-			</Sidebar>
-
-			<Sidebar
-				anchor={"right"}
-				open={openRight}
-				toggleDrawer={toggleDrawerRight}
+			{/* {state.markers !== null ? JSON.stringify(state.markers) : null} */}
+			<MapContainer
+				style={{ height: "92vh" }}
+				center={[18.5204, 73.8567]}
+				zoom={13}
+				scrollWheelZoom={true}
+				doubleClickZoom={false}
+				whenCreated={(map) => {
+					setState((prevState) => ({
+						...prevState,
+						map: map,
+					}));
+				}}
 			>
-				<div className="right-sidebar">
-					<h1 style={{ color: "white" }}>Info</h1>
-					<div className="tags-info"></div>
-				</div>
-			</Sidebar>
+				<LayersControl position="topright">
+					<LayersControl.BaseLayer checked name="OSM">
+						<TileLayer
+							attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+						/>
+					</LayersControl.BaseLayer>
+					<LayersControl.BaseLayer name="MapTiler Street Map">
+						<TileLayer
+							attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+							url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ZwUohaY0M43TShPZZw1q"
+						/>
+					</LayersControl.BaseLayer>
+					<LayersControl.BaseLayer name="Google Map">
+						<TileLayer
+							attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+							url="http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga"
+						/>
+					</LayersControl.BaseLayer>
+					<LayersControl.BaseLayer name="MapTiler Topography">
+						<TileLayer
+							attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+							url="https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=ZwUohaY0M43TShPZZw1q"
+						/>
+					</LayersControl.BaseLayer>
+				</LayersControl>
+				<GeoSearchControlElement
+					provider={prov}
+					showMarker={true}
+					showPopup={false}
+					maxMarkers={3}
+					retainZoomLevel={false}
+					animateZoom={true}
+					autoClose={false}
+					searchLabel={"Enter address, please"}
+					keepResult={true}
+					popupFormat={({ query, result }) => result.label}
+				/>
 
-			<button className="btn btn-primary" onClick={toggleDrawerLeft}>
-				Left
-			</button>
-			<button className="btn btn-primary" onClick={toggleDrawerRight}>
-				Right
-			</button>
+				<LocationMarker />
+
+				{state.markers.length > 0 ? (
+					<div
+						className="container-fluid"
+						style={{ position: "absolute", bottom: 15, zIndex: 314159 }}
+					>
+						<button className="btn btn-danger" onClick={resetMarkers}>
+							Reset
+						</button>
+
+						{state.markers.length > 1 ? (
+							<button
+								className="btn btn-primary ms-2"
+								onClick={() => {
+									setFlag(true);
+									getRoute(state.map);
+								}}
+							>
+								Get Route
+							</button>
+						) : null}
+					</div>
+				) : null}
+			</MapContainer>
 		</>
 	);
 };
+
 export default GeoTag;
